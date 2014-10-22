3104b5db6bbbde7bcce06413c524a83a9bee7d1f
hidenorigoto <hidenorigoto@gmail.com>
2014-10-22 22:50:40 +0900

    initial commit

---
+++ src/Data/Data.php
@@ -0,0 +1,46 @@
+<?php
+/*
+ * Copyright (c) 2014 GOTO Hidenori <hidenorigoto@gmail.com>,
+ * All rights reserved.
+ *
+ * This file is part of Domain Coder.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace DomainCoder\Model\Data;
+
+class Data
+{
+    /**
+     * @var string
+     */
+    public $id;
+    /**
+     * @var string
+     */
+    public $name;
+    /**
+     * @var string
+     */
+    public $tableName;
+    /**
+     * @var string
+     */
+    public $description;
+    /**
+     * @var Field[]
+     */
+    public $fields;
+}
